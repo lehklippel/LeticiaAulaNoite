a6322065eacd625d6055d777e3b66bbe8a7a4cba
Letícia Klippel Boero <144861233+lehklippel@users.noreply.github.com>
2023-09-13 19:19:41 -0300

    Add files via upload

---
+++ Condicionais/index.js
@@ -0,0 +1,37 @@
+
+/*let numero1 = 27
+let numero2 = 49
+if (numero1 > numero2) {
+    console.log ("O número 1 é maior que o 2")
+} else if (numero1 < numero2) {
+    console.log ("O número 1 é menor que o 2")
+} else {
+    console.log ("O número 1 é igual ao 2")
+}*/
+/*
+let pokemInicial = "Squirtle"
+switch (pokemInicial){
+    case "Bulbasauro":
+        console.log("Seu pokemon é estilo planta e veneno.")
+        break
+    case "Charmander":
+        console.log("Seu pokemon é do tipo fogo.")
+        break
+    default:
+        console.log("Seu pokemin é estilo água.")
+        break
+}
+*/
+/*
+let concluiuOEnsinoMédio = prompt ("Você concluiu o ensino médio? (Sim ou não)")
+let maiorIdade = Number(prompt("Qual sua idade?"))
+let cusandoFaculdade = prompt ("Você está fazendo faculdade? (Sim ou não)")
+
+function verificarFacul (EnsinoMedio, Idade, Faculdade){
+if (EnsinoMedio === "Sim" && Idade >= 18 && Faculdade === "Não"){
+    console.log ("Pode cursar faculdade")
+} else{
+    console.log ("Você não pode cursar")
+}
+}
+verificarFacul(concluiuOEnsinoMédio, maiorIdade, cusandoFaculdade)*/
